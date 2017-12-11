e00a0ec9a698c4593aabd44fb7817543ac05eaec
ivan-comar <guardian.sanddy@gmail.com>
2017-12-11 10:18:04 +0800

    add reducers/index file

---
+++ material/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import settings from './settings';
+
+
+const reducers = {
+  routing: routerReducer,
+  settings
+};
+
+module.exports = combineReducers(reducers);
