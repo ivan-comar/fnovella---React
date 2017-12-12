ad94e20b977ba7d832d0f192656cbc571588c28f
ivan-comar <guardian.sanddy@gmail.com>
2017-12-12 21:50:54 +0800

    add routes/catalog/components/catalog file

---
+++ material/src/routes/app/routes/catalog/components/Catalog.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import QueueAnim from 'rc-queue-anim';
+import EditForm from './EditForm';
+import ListElements from './ListElements';
+
+const optionsName = 'Catalogo';
+
+class MainOptions extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <article className="article padding-lg-v article-bordered">
+        <div className="container-fluid with-maxwidth">
+          <div className="row">
+            <div className="col-xl-3">
+
+              <div className="box-body">
+                <div className="icon-box ibox-plain ibox-center">
+                  <div className="ibox-icon">
+                    <a href="javascript:;"><i className="material-icons">dashboard</i></a>
+                  </div>
+                  <h5>{optionsName}s</h5>
+                </div>
+              </div>
+
+            </div>
+            <div className="col-xl-9">
+              <div className="row">
+
+                <div className="col-xl-4">
+                  <div className="box box-default">
+                    <div className="box-body">
+                      <div onClick={() => this.props.changeView("ADD_ELEMENT") } className="icon-box ibox-plain ibox-center">
+                        <div className="ibox-icon">
+                          <a href="javascript:;"><i className="material-icons">add</i></a>
+                        </div>
+                        <h6>Agregar {optionsName}</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-xl-4">
+                  <div className="box box-default">
+                    <div className="box-body">
+                      <div onClick={() => this.props.changeView("VIEW_ELEMENT") } className="icon-box ibox-plain ibox-center">
+                        <div className="ibox-icon">
+                          <a><i className="material-icons">mode_edit</i></a>
+                        </div>
+                        <h6>Editar {optionsName}</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="col-xl-4">
+                  <div className="box box-default">
+                    <div className="box-body">
+                      <div className="icon-box ibox-plain ibox-center">
+                        <div className="ibox-icon">
+                          <a href="javascript:;"><i className="material-icons">remove</i></a>
+                        </div>
+                        <h6>Eliminar {optionsName}</h6>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </article>
+    )
+  };
+}
+
+
+
+
+class Catalog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: "ADD_ELEMENT"
+    };
+    this.changeView = this.changeView.bind(this); //bind this element
+  }
+
+  changeView(data){
+    this.setState({ active: data });
+  }
+
+  activeView() {
+    switch(this.state.active) {
+      case 'ADD_ELEMENT':
+        return <EditForm />;
+      case "VIEW_ELEMENT":
+        return <ListElements />;
+      default:
+        return null;
+    }
+  }
+  render() {
+      return (
+        <div className="container-fluid no-breadcrumbs page-dashboard">
+
+          <QueueAnim type="bottom" className="ui-animate">
+            <div key="1"><MainOptions changeView={ this.changeView } /></div>
+            <hr/>
+            <div key="2">{ this.activeView() }</div>
+          </QueueAnim>
+
+        </div>
+      );
+  }
+}
+
+module.exports = Catalog;
